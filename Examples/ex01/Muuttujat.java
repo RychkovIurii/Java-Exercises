8158da15115bb5f96a6f4d461a93f7e294e818cb
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-03-20 00:01:19 +0200

    Variables

---
+++ Examples/ex01/Muuttujat.java
@@ -0,0 +1,18 @@
+package ex01;
+
+public class Muuttujat {
+    public static void main(String[] args)
+    {
+        String text = "Consider the following example...";
+        int number = -125;
+        boolean isNight = true;
+        float decimals = 14.8888887f;
+        char c = 'r';
+
+        System.out.println(text);
+        System.out.println(number);
+        System.out.println(isNight);
+        System.out.println(decimals);
+        System.out.println(c);
+    }
+}
